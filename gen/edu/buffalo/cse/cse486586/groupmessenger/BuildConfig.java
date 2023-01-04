cf5ed7ce7ac0dc60f5f4ab4cbd142c0bf722d899
Elsie Urena <71279445+elsieu9700@users.noreply.github.com>
2023-01-04 13:29:10 -0400

    Delete gen/edu/buffalo/cse/cse486586/groupmessenger directory

--- gen/edu/buffalo/cse/cse486586/groupmessenger/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.buffalo.cse.cse486586.groupmessenger;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
